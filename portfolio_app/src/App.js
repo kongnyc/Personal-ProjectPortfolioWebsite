4de218fb30ada28dd238ad69f18e1d88826ceefa
kongnyc <bbsyoow@gmail.com>
2020-03-28 17:02:51 -0400

    start app

---
+++ portfolio_app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CONGSONG YANG & Project Portfolio Website </h1>
+    </div>
+  );
+}
+
+export default App;
